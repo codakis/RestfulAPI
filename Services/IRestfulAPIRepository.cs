d5f2a692acd4e811342bcc0b8dca115415184eda
Akis Kontakis <akis@inked.gr>
2020-12-02 15:13:54 +0200

    Skeleton of restful

---
+++ Services/IRestfulAPIRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using RestfulAPI.Entities;
+
+namespace RestfulAPI.Services
+{
+    public interface IRestfulApiRepository
+    {
+        void AddCourse(Guid authorId, Course course);
+        void DeleteCourse(Course course);
+        Course GetCourse(Guid authorId, Guid courseId);
+        IEnumerable<Course> GetCourses(Guid authorId);
+        void UpdateCourse(Course course);
+        void AddAuthor(Author author);
+        bool AuthorExists(Guid authorId);
+        void DeleteAuthor(Author author);
+        Author GetAuthor(Guid authorId);
+        IEnumerable<Author> GetAuthors();
+        IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds);
+        void UpdateAuthor(Author author);
+        bool Save();
+        void Dispose();
+    }
+}
